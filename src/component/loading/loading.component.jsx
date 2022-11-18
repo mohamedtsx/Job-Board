9237f338f97a98d1c689443634e667e3dc4aef6c
Mohamed <ma0068664@gmail.com>
2022-11-18 11:19:22 +0200

    _

--- src/component/loading/loading.component.jsx
+++ src/component/loading/loading.component.jsx
@@ -12,26 +12,27 @@ import './loading.style.css';
 const Loading = () => {
     const [ active, setActive ] = useState(true);
 
+    // when user choose an account error message has shown
 
-    useEffect(() => {
-        setTimeout(() => {
-            setActive(false);      
-        }, 6000);
-    }, [])
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setActive(false);      
+    //     }, 6000);
+    // }, [])
 
 
     return(
-        active ? (
+        // active ? (
             <div className='loader-container'>
                 <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
                     <circle className="path" fill="none" stroke='#ddd' strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
                 </svg>
             </div>
-        ) : (
-            <div className='over-loading-message'>
-                <h1>Error Message</h1>
-            </div>
-        )
+        // ) : (
+        //     <div className='over-loading-message'>
+        //         <h1>Error Message</h1>
+        //     </div>
+        // )
     )
 }
 
